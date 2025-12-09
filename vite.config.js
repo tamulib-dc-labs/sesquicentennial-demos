3d8ddbc2651f239ae99072d26734c0bc812d832d
Mark Baggett <markpbaggett@gmail.com>
2025-12-09 16:32:19 -0600

    Update vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/sesquicentennial-demos/',
-  plugins: [/* ... */]
+  plugins: [/* ... */],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        map: resolve(__dirname, 'map.html'),
+        sample: resolve(__dirname, 'sample.html')
+      }
+    }
+  }
 })
